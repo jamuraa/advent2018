a1043dfbbb31fedd9443a855f32da3669fc7799c
Marie Janssen <jamuraa@base0.net>
2018-12-04 23:03:26 -0800

    use String manip in day5 for speed

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,17 +1,23 @@
 use std::{
-    collections::HashMap,
     fs::File,
     io::{self, prelude::*, BufReader},
 };
 
-fn react(chars: &mut Vec<char>) {
+fn pair(a: u8, b: u8) -> bool {
+    if a < 97 {
+        a + 32 == b
+    } else {
+        a - 32 == b
+    }
+}
+
+fn react(chars: &mut String) {
     let mut i = 0;
     while i != chars.len() - 1 {
-        let x = chars[i];
-        let y = chars[i + 1];
-        if x.eq_ignore_ascii_case(&y) && (x.is_lowercase() != y.is_lowercase()) {
-            chars.remove(i);
-            chars.remove(i);
+        let x = chars.as_bytes()[i];
+        let y = chars.as_bytes()[i + 1];
+        if pair(x, y) {
+            chars.replace_range(i..i + 2, "");
             if chars.len() == 0 {
                 return;
             }
@@ -24,12 +30,10 @@ fn react(chars: &mut Vec<char>) {
     }
 }
 
-fn without_polymer(chars: &Vec<char>, rem: &char) -> Vec<char> {
-    chars
-        .iter()
-        .filter(|x| !x.eq_ignore_ascii_case(rem))
-        .map(|x| x.clone())
-        .collect()
+fn without_polymer(chars: &String, rem: &char) -> String {
+    let mut ret = chars.clone();
+    ret.retain(|x| !x.eq_ignore_ascii_case(rem));
+    ret
 }
 
 fn main() -> io::Result<()> {
@@ -37,7 +41,7 @@ fn main() -> io::Result<()> {
     let reader = BufReader::new(f);
 
     let mut lines = reader.lines();
-    let chars: Vec<char> = lines.next().unwrap().unwrap().chars().collect();
+    let chars: String = lines.next().unwrap().unwrap();
     let mut original = chars.clone();
 
     react(&mut original);
@@ -69,12 +73,12 @@ mod tests {
 
     #[test]
     fn test_react() {
-        let mut a = "AcCb".chars().collect();
-        let a2: Vec<char> = "Ab".chars().collect();
+        let mut a = "AcCb".to_string();
+        let a2 = "Ab";
         react(&mut a);
         assert_eq!(a2, a);
-        let mut c = "dabAcCaCBAcCcaDA".chars().collect();
-        let c4: Vec<char> = "dabCBAcaDA".chars().collect();
+        let mut c = "dabAcCaCBAcCcaDA".to_string();
+        let c4 = "dabCBAcaDA";
         react(&mut c);
         assert_eq!(c4, c);
         react(&mut c);
@@ -83,14 +87,14 @@ mod tests {
 
     #[test]
     fn test_without() {
-        let a: Vec<char> = "dabAcCaCBAcCcaDA".chars().collect();
-        let a2: Vec<char> = "dbcCCBcCcD".chars().collect();
+        let a = "dabAcCaCBAcCcaDA".to_string();
+        let a2 = "dbcCCBcCcD";
         assert_eq!(a2, without_polymer(&a, &'a'));
-        let a3: Vec<char> = "daAcCaCAcCcaDA".chars().collect();
+        let a3 = "daAcCaCAcCcaDA";
         assert_eq!(a3, without_polymer(&a, &'b'));
-        let a4: Vec<char> = "dabAaBAaDA".chars().collect();
+        let a4 = "dabAaBAaDA";
         assert_eq!(a4, without_polymer(&a, &'c'));
-        let a5: Vec<char> = "abAcCaCBAcCcaA".chars().collect();
+        let a5 = "abAcCaCBAcCcaA";
         assert_eq!(a5, without_polymer(&a, &'d'));
     }
 
